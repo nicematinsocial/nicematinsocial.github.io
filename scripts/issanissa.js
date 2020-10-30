5589a05c34fe0dd525d3b481913109bc52665b3b
Nice Matin Social <73704378+nicematinsocial@users.noreply.github.com>
2020-10-30 18:48:58 +0100

    Update issanissa.js

--- scripts/issanissa.js
+++ scripts/issanissa.js
@@ -102,10 +102,10 @@ const fetchTwetches = async(sdk, selOrder) => {
         orderBy = 'orderBy: LIKES_BY_POST_ID__COUNT_DESC';
     };
     
-    let botList = 'in: ["652","4603"]';
+    //let botList = 'in: ["652","4603"]';
         
     response = await sdk.query(`{
-  allPosts(filter: {bContent: {endsWith: "$nce"}, and: {userId: {${botList}}}}, ${orderBy}) {
+  allPosts(filter: {bContent: {endsWith: "$nce"}}, ${orderBy}) {
     nodes {
       bContent
       createdAt
